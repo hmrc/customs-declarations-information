5e3531186f94fdabfb4dd43df567c0636ddc27b5
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2021-06-18 09:57:51 +0100

    CDD-3291_process_response (#103)

--- test/util/StatusTestXMLData.scala
+++ test/util/StatusTestXMLData.scala
@@ -301,42 +301,6 @@ object StatusTestXMLData {
       </od:GoodsShipment>
     </od:Declaration>
 
-  def invalidStatusResponse(declarationNode: NodeSeq): NodeSeq =
-    <n1:queryDeclarationInformationResponse
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xmlns:xsd_1="http://trade.core.ecf/messages/2017/03/31/"
-      xmlns:n1="http://gov.uk/customs/retrieveDeclarationInformation/v1"
-      xmlns:tns="http://cmm.core.ecf/BaseTypes/cmmPartyTypes/trade/2017/02/22/"
-      xmlns:n2="http://cmm.core.ecf/BaseTypes/cmmServiceTypes/trade/2017/02/22/"
-      xmlns:n3="http://cmm.core.ecf/BaseTypes/cmmDeclarationTypes/trade/2017/02/22/"
-      xmlns:tns_3="http://cmm.core.ecf/BaseTypes/cmmEnhancementTypes/trade/2017/02/22/"
-      xsi:schemaLocation="http://gov.uk/customs/retrieveDeclarationInformation/v1 queryDeclarationInformationResponse.xsd">
-      <n1:responseCommon>
-        <n1:processingDate>2001-12-17T09:30:47Z</n1:processingDate>
-      </n1:responseCommon>
-      <n1:responseDetail>
-        <n1:declarationManagementInformationResponse>
-          <n2:extensions>
-            <tns_3:value>String</tns_3:value>
-            <tns_3:type>token</tns_3:type>
-          </n2:extensions>
-          <n2:sequenceNumber>0</n2:sequenceNumber>
-          <n2:request>
-            <n2:status>token</n2:status>
-            <n2:id>String</n2:id>
-            <n2:sequenceNumber>0</n2:sequenceNumber>
-            <n2:timeStamp>2001-12-17T09:30:47Z</n2:timeStamp>
-            <n2:externalId>String</n2:externalId>
-          </n2:request>
-          <n2:id>String</n2:id>
-          <n2:timeStamp>2001-12-17T09:30:47Z</n2:timeStamp>
-          <n2:isFinal>true</n2:isFinal>
-          <n2:externalId>String</n2:externalId>
-            {declarationNode}
-        </n1:declarationManagementInformationResponse>
-      </n1:responseDetail>
-    </n1:queryDeclarationInformationResponse>
-
   def generateDeclarationStatusResponseContainingAllOptionalElements(acceptanceOrCreationDate: DateTime): NodeSeq = {
     val content = generateDeclarationStatusDetailsElement(generateHMRCDeclaration(acceptanceOrCreationDate, withOptionalElements = true), getWcoDeclarationWithAllElementsPopulated())
 
