be4d2024e1850c849c048a5a7da82d24ce36af43
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2021-07-20 14:44:08 +0100

    CDD-3296: added component test for search endpoint and schema validation check to status and version endpoints (#122)

--- test/component/CustomsDeclarationSearchSpec.scala
+++ test/component/CustomsDeclarationSearchSpec.scala
@@ -36,7 +36,7 @@ import javax.xml.transform.stream.StreamSource
 import javax.xml.validation.Schema
 import scala.concurrent.Future
 
-class CustomsDeclarationVersionSpec extends ComponentTestSpec
+class CustomsDeclarationSearchSpec extends ComponentTestSpec
   with AuditService
   with ExpectedTestResponses
   with Matchers
@@ -45,10 +45,8 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
   with ApiSubscriptionFieldsService
   with AuthService {
 
-  private val mrn = "some-mrn"
-
-  private val endpointMRN = s"/mrn/$mrn/version"
-  private val endpointMissingMRN = s"/mrn//version"
+  private val endpointAllValid = "/search?eori=GB123456789000&partyRole=submitter&declarationCategory=IM&goodsLocationCode=BELBELOB4&declarationStatus=all&dateFrom=2021-04-01&dateTo=2021-04-04&pageNumber=2"
+  private val endpointInvalidPartyRole = "/search?eori=GB123456789000&partyRole=invalid&declarationCategory=IM&goodsLocationCode=BELBELOB4&declarationStatus=all&dateFrom=2021-04-01&dateTo=2021-04-04&pageNumber=2"
 
   private val apiSubscriptionKeyForXClientId =
     ApiSubscriptionKey(clientId = clientId, context = "customs%2Fdeclarations-information", version = VersionOne)
@@ -56,89 +54,73 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
   protected val xsdErrorLocationV1: String = "/api/conf/1.0/schemas/customs/error.xsd"
   private val schemaErrorV1: Schema = ValidateXmlAgainstSchema.getSchema(xsdErrorLocationV1).get
 
-  protected val xsdResponseLocation: String = "/api/conf/1.0/schemas/wco/declaration/DeclarationInformationRetrievalVersionResponse.xsd"
+  protected val xsdResponseLocation: String = "/api/conf/1.0/schemas/wco/declaration/DeclarationInformationRetrievalSearchResponse.xsd"
   private val schemaResponse: Schema = ValidateXmlAgainstSchema.getSchema(xsdResponseLocation).get
 
   private def validResponse() =
-    """<p:DeclarationVersionResponse xsi:schemaLocation="http://gov.uk/customs/retrieveDeclarationVersion" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:p4="urn:un:unece:uncefact:data:standard:UnqualifiedDataType:6" xmlns:p3="urn:wco:datamodel:WCO:Declaration_DS:DMS:2" xmlns:p2="urn:wco:datamodel:WCO:DEC-DMS:2" xmlns:p1="urn:wco:datamodel:WCO:Response_DS:DMS:2" xmlns:p="http://gov.uk/customs/retrieveDeclarationVersion">
-      |      <p:DeclarationVersionDetails>
+    """<p:DeclarationSearchResponse xsi:schemaLocation="http://gov.uk/customs/declarationInformationRetrieval/declarationSummary/v1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:p4="urn:un:unece:uncefact:data:standard:UnqualifiedDataType:6" xmlns:p3="urn:wco:datamodel:WCO:Declaration_DS:DMS:2" xmlns:p2="urn:wco:datamodel:WCO:DEC-DMS:2" xmlns:p1="urn:wco:datamodel:WCO:Response_DS:DMS:2" xmlns:p="http://gov.uk/customs/declarationInformationRetrieval/declarationSummary/v1">
+      |      <p:DeclarationSearchDetails>
       |          <p:Declaration>
       |                <p:ID>18GB9JLC3CU1LFGVR2</p:ID>
-      |                <p:VersionID>2</p:VersionID>
-      |                <p:CreatedDateTime>
+      |                <p:ReceivedDateTime>
       |                  <p:DateTimeString formatCode="304">20190702110757Z</p:DateTimeString>
-      |                </p:CreatedDateTime>
+      |                </p:ReceivedDateTime>
+      |                <p:ROE>6</p:ROE>
+      |                <p:ICS>15</p:ICS>
       |                <p:LRN>20GBAKZ81EQJ2WXYZ</p:LRN>
       |              </p:Declaration>
       |          <p2:Declaration>
       |                <p2:FunctionCode>9</p2:FunctionCode>
       |                <p2:TypeCode>IM</p2:TypeCode>
+      |                <p2:Submitter>
+      |                  <p2:ID>GB123456789012000</p2:ID>
+      |                </p2:Submitter>
+      |                <p2:Declarant>
+      |                  <p2:ID>GB123456789012000</p2:ID>
+      |                </p2:Declarant>
       |                <p2:GoodsShipment>
+      |                  <p2:Consignee>
+      |                    <p2:ID>GB123456789012123</p2:ID>
+      |                  </p2:Consignee>
       |                  <p2:Consignment>
       |                    <p2:GoodsLocation>
       |                      <p2:ID>LIVLIVLIVR</p2:ID>
       |                      <p2:TypeCode>14</p2:TypeCode>
       |                    </p2:GoodsLocation>
       |                  </p2:Consignment>
+      |                  <p2:Consignor>
+      |                    <p2:ID>GB123456789012987</p2:ID>
+      |                  </p2:Consignor>
       |                  <p2:PreviousDocument>
       |                    <p2:ID>18GBAKZ81EQJ2FGVR</p2:ID>
       |                    <p2:TypeCode>DCR</p2:TypeCode>
       |                  </p2:PreviousDocument>
       |                  <p2:PreviousDocument>
       |                    <p2:ID>18GBAKZ81EQJ2FGVA</p2:ID>
-      |                    <p2:TypeCode>DCS</p2:TypeCode>
+      |                    <p2:TypeCode>MCR</p2:TypeCode>
       |                  </p2:PreviousDocument>
       |                </p2:GoodsShipment>
       |              </p2:Declaration>
-      |        </p:DeclarationVersionDetails><p:DeclarationVersionDetails>
-      |          <p:Declaration>
-      |                <p:ID>18GB9JLC3CU1LFGVR3</p:ID>
-      |                <p:VersionID>1</p:VersionID>
-      |                <p:CreatedDateTime>
-      |                  <p:DateTimeString formatCode="304">20190703120957Z</p:DateTimeString>
-      |                </p:CreatedDateTime>
-      |                <p:LRN>30GBAKZ81EQJ2WXYZ</p:LRN>
-      |              </p:Declaration>
-      |          <p2:Declaration>
-      |                <p2:FunctionCode>9</p2:FunctionCode>
-      |                <p2:TypeCode>IM</p2:TypeCode>
-      |                <p2:GoodsShipment>
-      |                  <p2:Consignment>
-      |                    <p2:GoodsLocation>
-      |                      <p2:ID>LIVLIVLIVR</p2:ID>
-      |                      <p2:TypeCode>15</p2:TypeCode>
-      |                    </p2:GoodsLocation>
-      |                  </p2:Consignment>
-      |                  <p2:PreviousDocument>
-      |                    <p2:ID>18GBAKZ81EQJ2FGVX</p2:ID>
-      |                    <p2:TypeCode>DCR</p2:TypeCode>
-      |                  </p2:PreviousDocument>
-      |                  <p2:PreviousDocument>
-      |                    <p2:ID>18GBAKZ81EQJ2FGVZ</p2:ID>
-      |                    <p2:TypeCode>DCS</p2:TypeCode>
-      |                  </p2:PreviousDocument>
-      |                </p2:GoodsShipment>
-      |              </p2:Declaration>
-      |        </p:DeclarationVersionDetails>
-      |    </p:DeclarationVersionResponse>""".stripMargin
+      |        </p:DeclarationSearchDetails><p:CurrentPageNumber>1</p:CurrentPageNumber>
+      |        <p:TotalResultsAvailable>1</p:TotalResultsAvailable>
+      |        <p:TotalPagesAvailable>1</p:TotalPagesAvailable>
+      |        <p:NoResultsReturned>false</p:NoResultsReturned>
+      |    </p:DeclarationSearchResponse>""".stripMargin
 
-  val missingMrnResponse =
-    """<?xml version='1.0' encoding='UTF-8'?>
-         |<errorResponse>
-         |      <code>BAD_REQUEST</code>
-         |      <message>Missing MRN parameter</message>
-         |
-         |    </errorResponse>""".stripMargin
+  private val invalidPartyRoleResponse =
+    """<errorResponse>
+      |<code>CDS60006</code>
+      |<message>Invalid partyRole parameter</message>
+      |</errorResponse>""".stripMargin
 
 
   private def createFakeRequest(endpoint: String, headers: Map[String, String] = ValidHeaders): FakeRequest[AnyContentAsEmpty.type] =
     FakeRequest("GET", endpoint).withHeaders(headers.-(CONTENT_TYPE).toSeq: _*)
 
-  val validCspRequest: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpointMRN).fromCsp
-  val validNonCspRequest: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpointMRN).fromNonCsp
-  val validNonCspRequestV2: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpoint = endpointMRN, headers = ValidHeaders + ACCEPT_HMRC_XML_HEADER_V2).fromNonCsp
-  val validMrnRequest: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpointMRN).fromCsp
-  val missingMrnRequest: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpointMissingMRN).fromCsp
+  val validCspRequest: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpointAllValid).fromCsp
+  val validNonCspRequest: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpointAllValid).fromNonCsp
+  val validNonCspRequestV2: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpoint = endpointAllValid, headers = ValidHeaders + ACCEPT_HMRC_XML_HEADER_V2).fromNonCsp
+  val invalidPartyRoleRequest: FakeRequest[AnyContentAsEmpty.type] = createFakeRequest(endpointInvalidPartyRole).fromCsp
 
   override protected def beforeAll() {
     startMockServer()
@@ -158,7 +140,7 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
 
       implicit lazy val app: Application = super.app(configMap + ("shutter.v1" -> "true"))
 
-      startBackendVersionServiceV1()
+      startBackendSearchServiceV1()
       startApiSubscriptionFieldsService(apiSubscriptionKeyForXClientId)
 
       And("the CSP is authorised with its privileged application")
@@ -177,9 +159,9 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
     }
 
   feature("Declaration Information API authorises version requests from CSPs with v1.0 accept header") {
-    scenario("An authorised CSP successfully requests version") {
-      Given("A CSP wants the version of a declaration")
-      startBackendVersionServiceV1()
+    scenario("An authorised CSP successfully requests a search") {
+      Given("A CSP wants to search for a declaration")
+      startBackendSearchServiceV1()
       startApiSubscriptionFieldsService(apiSubscriptionKeyForXClientId)
 
       And("the CSP is authorised with its privileged application")
@@ -191,7 +173,7 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
       Then("a response with a 200 (OK) status is received")
       status(result) shouldBe OK
 
-      And("the response body is a valid version xml")
+      And("the response body is a valid search xml")
       contentAsString(result) shouldBe validResponse
       schemaResponse.newValidator().validate(new StreamSource(new StringReader(contentAsString(result))))
 
@@ -199,14 +181,14 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
       eventually(verifyAuthServiceCalledForCsp())
 
       And("v1 config was used")
-      eventually(verify(1, postRequestedFor(urlEqualTo(CustomsDeclarationsExternalServicesConfig.BackendVersionDeclarationServiceContextV1))))
+      eventually(verify(1, postRequestedFor(urlEqualTo(CustomsDeclarationsExternalServicesConfig.BackendSearchDeclarationServiceContextV1))))
     }
   }
 
   feature("Declaration Information API authorises version requests from non-CSPs with v1.0 accept header") {
-    scenario("An authorised non-CSP successfully requests version") {
-      Given("A non-CSP wants the version of a declaration")
-      startBackendVersionServiceV1()
+    scenario("An authorised non-CSP successfully requests search") {
+      Given("A non-CSP wants to search for a declaration")
+      startBackendSearchServiceV1()
 
       And("the non-CSP is authorised with its standard application")
       authServiceUnauthorisesScopeForCSPWithoutRetrievals(nonCspBearerToken)
@@ -226,16 +208,16 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
       eventually(verifyAuthServiceCalledForNonCsp())
 
       And("v1 config was used")
-      eventually(verify(1, postRequestedFor(urlEqualTo(CustomsDeclarationsExternalServicesConfig.BackendVersionDeclarationServiceContextV1))))
+      eventually(verify(1, postRequestedFor(urlEqualTo(CustomsDeclarationsExternalServicesConfig.BackendSearchDeclarationServiceContextV1))))
     }
   }
   
   feature("Declaration Information API authorises version requests from non-CSPs with v2.0 accept header") {
-    scenario("An authorised non-CSP successfully requests version") {
-      Given("A non-CSP wants the version of a declaration")
-      startBackendVersionServiceV2()
+    scenario("An authorised non-CSP successfully requests search") {
+      Given("A non-CSP wants the versions of a declaration")
+      startBackendSearchServiceV2()
 
-      And("the non-CSP is authorised with its standard application")
+      And("the non-CSP is authorised with its privileged application")
       authServiceUnauthorisesScopeForCSPWithoutRetrievals(nonCspBearerToken)
       authServiceAuthorizesNonCspWithEori()
 
@@ -253,28 +235,28 @@ class CustomsDeclarationVersionSpec extends ComponentTestSpec
       eventually(verifyAuthServiceCalledForNonCsp())
 
       And("v2 config was used")
-      eventually(verify(1, postRequestedFor(urlEqualTo(CustomsDeclarationsExternalServicesConfig.BackendVersionDeclarationServiceContextV2))))
+      eventually(verify(1, postRequestedFor(urlEqualTo(CustomsDeclarationsExternalServicesConfig.BackendSearchDeclarationServiceContextV2))))
     }
   }
 
-  feature("Declaration Information API query declaration version by MRN") {
-    scenario("An authorised CSP queries declaration version with missing MRN value") {
-      Given("A CSP omits the MRN")
-      startBackendVersionServiceV1()
+  feature("Declaration Information API query declaration search") {
+    scenario("An authorised CSP queries declaration version with an invalid partyRole parameter") {
+      Given("A CSP submits an invalid partyRole")
+      startBackendSearchServiceV1()
       startApiSubscriptionFieldsService(apiSubscriptionKeyForXClientId)
 
       And("the CSP is authorised with its privileged application")
       authServiceAuthorizesCSP()
 
-      When("a MRN request with data is sent to the API")
-      val result: Future[Result] = route(app = app, missingMrnRequest).value
+      When("a request with an invalid partyRole is sent to the API")
+      val result: Future[Result] = route(app = app, invalidPartyRoleRequest).value
 
       Then("a response with a 400 status is received")
       status(result) shouldBe BAD_REQUEST
 
-      And("the response body is an error version xml")
-      stringToXml(contentAsString(result)) shouldBe stringToXml(missingMrnResponse)
-      schemaErrorV1.newValidator().validate(new StreamSource(new StringReader(missingMrnResponse)))
+      And("the response body is an error xml payload")
+      stringToXml(contentAsString(result)) shouldBe stringToXml(invalidPartyRoleResponse)
+      schemaErrorV1.newValidator().validate(new StreamSource(new StringReader(invalidPartyRoleResponse)))
     }
   }
 }
