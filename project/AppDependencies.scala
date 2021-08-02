9c85d07f993fcf5f5708453f628898260dc029d7
Rashad Mughal <rashadmughal@users.noreply.github.com>
2021-08-02 16:30:39 +0100

    Code cleanup

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ object AppDependencies {
   private val scalatestplusVersion = "5.1.0"
   private val mockitoVersion = "3.11.2"
   private val wireMockVersion = "2.29.1"
-  private val playJsonJodaVersion = "2.8.1"
   private val customsApiCommonVersion = "1.57.0"
   private val testScope = "test,it"
 
@@ -19,8 +18,7 @@ object AppDependencies {
 
   val customsApiCommonTests = "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion % testScope classifier "tests"
 
-  val playJsonJoda = "com.typesafe.play" %% "play-json-joda" % playJsonJodaVersion
-
   val flexmark = "com.vladsch.flexmark" % "flexmark-all" % "0.35.10"
+
   val jacksonModule = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.2"
 }
