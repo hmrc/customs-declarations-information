b3123a4baa68cf52b062b112634477191874d49c
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-02-07 14:33:43 +0000

    Addressed pr-bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,12 @@ object AppDependencies {
 
   private val testScope = "test,it"
 
-  val scalaTestPlusPlay     = "org.scalatestplus.play"       %% "scalatestplus-play"   % "5.1.0"  % testScope
-  val wireMock              = "com.github.tomakehurst"       %  "wiremock-jre8"        % "2.34.0" % testScope
-  val mockito               = "org.mockito"                  %  "mockito-core"         % "4.8.1"  % testScope
-  val customsApiCommon      = "uk.gov.hmrc"                  %% "customs-api-common"   % "1.57.0" withSources()
-  val customsApiCommonTests = "uk.gov.hmrc"                  %% "customs-api-common"   % "1.57.0" % testScope classifier "tests"
-  val flexmark              = "com.vladsch.flexmark"         %  "flexmark-all"         % "0.35.10"
-  val jacksonModule         = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.4"
+  val scalaTestPlusPlay     = "org.scalatestplus.play"       %% "scalatestplus-play"     % "5.1.0"   % testScope
+  val wireMock              = "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.34.0"  % testScope
+  val mockito               = "org.mockito"                  %  "mockito-core"           % "4.8.1"   % testScope
+  val customsApiCommon      = "uk.gov.hmrc"                  %% "customs-api-common"     % "1.57.0"  withSources()
+  val customsApiCommonTests = "uk.gov.hmrc"                  %% "customs-api-common"     % "1.57.0"  % testScope classifier "tests"
+  val flexmark              = "com.vladsch.flexmark"         %  "flexmark-all"           % "0.35.10" % testScope
+  val jacksonModule         = "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.4"
+  val bootstrapTestPlay     = "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "7.13.0"  % testScope
 }
