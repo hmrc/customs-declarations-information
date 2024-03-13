ab7ce3990f77b06f2be4ac4c839c7cc8e6f17e9c
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-03-13 15:23:29 +0000

    DCWL-1833 moved dependencies into vals

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,10 +5,15 @@ object AppDependencies {
   private val testScope = "test,it"
   private val customsApiCommonVersion = "1.60.0"
 
-  val bootstrapBackendPlay28  = "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"   % "7.15.0"
-  val cats                    = "org.typelevel"                %% "cats-core"                   % "2.10.0"
-  val scalaTestPlusPlay       = "org.scalatestplus.play"       %% "scalatestplus-play"          % "5.1.0"     % testScope
-  val wireMock                = "com.github.tomakehurst"       %  "wiremock-standalone"         % "2.27.2"    % testScope
-  val mockito                 = "org.scalatestplus"            %% "mockito-3-4"                 % "3.2.10.0"  % testScope
-  val flexmark                = "com.vladsch.flexmark"         %  "flexmark-all"                % "0.35.10"   % testScope
+  val compile = Seq(
+  "uk.gov.hmrc"            %% "bootstrap-backend-play-28"   % "7.15.0",
+  "org.typelevel"          %% "cats-core"                   % "2.10.0"
+  )
+
+  val test = Seq(
+  "org.scalatestplus.play" %% "scalatestplus-play"   % "5.1.0"     % testScope,
+  "com.github.tomakehurst" %  "wiremock-standalone"  % "2.27.2"    % testScope,
+  "org.scalatestplus"      %% "mockito-3-4"          % "3.2.10.0"  % testScope,
+  "com.vladsch.flexmark"   %  "flexmark-all"         % "0.35.10"   % testScope
+  )
 }
