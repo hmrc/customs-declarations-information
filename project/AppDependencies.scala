89abc7b4405740fa6ea0cd0cc320a3ca6a2a1d96
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-03-14 16:01:54 +0000

    DCWL-1833 PR bot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,18 @@ object AppDependencies {
 
   private val testScope = "test,it"
   private val customsApiCommonVersion = "1.60.0"
+  private val bootstrapVersion = "7.15.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-28"   % "7.15.0",
+    "uk.gov.hmrc"            %% "bootstrap-backend-play-28"   % bootstrapVersion,
     "org.typelevel"          %% "cats-core"                   % "2.10.0"
   )
 
   val test = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play"   % "5.1.0"     % testScope,
-    "com.github.tomakehurst" %  "wiremock-standalone"  % "2.27.2"    % testScope,
-    "org.scalatestplus"      %% "mockito-3-4"          % "3.2.10.0"  % testScope,
-    "com.vladsch.flexmark"   %  "flexmark-all"         % "0.35.10"   % testScope
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"          % testScope,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion % testScope,
+    "com.github.tomakehurst" %  "wiremock-standalone"    % "2.27.2"         % testScope,
+    "org.scalatestplus"      %% "mockito-3-4"            % "3.2.10.0"       % testScope,
+    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.35.10"        % testScope
   )
 }
