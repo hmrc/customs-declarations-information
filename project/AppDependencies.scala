68c72349c67cda3dfb8c7f724dbb8375ff53fc28
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-03-14 12:52:17 +0000

    DCWL-1833 Formatting, indentation.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,14 +6,14 @@ object AppDependencies {
   private val customsApiCommonVersion = "1.60.0"
 
   val compile = Seq(
-  "uk.gov.hmrc"            %% "bootstrap-backend-play-28"   % "7.15.0",
-  "org.typelevel"          %% "cats-core"                   % "2.10.0"
+    "uk.gov.hmrc"            %% "bootstrap-backend-play-28"   % "7.15.0",
+    "org.typelevel"          %% "cats-core"                   % "2.10.0"
   )
 
   val test = Seq(
-  "org.scalatestplus.play" %% "scalatestplus-play"   % "5.1.0"     % testScope,
-  "com.github.tomakehurst" %  "wiremock-standalone"  % "3.0.1"    % testScope,
-  "org.scalatestplus"      %% "mockito-3-4"          % "3.2.10.0"  % testScope,
-  "com.vladsch.flexmark"   %  "flexmark-all"         % "0.35.10"   % testScope
+    "org.scalatestplus.play" %% "scalatestplus-play"   % "5.1.0"     % testScope,
+    "com.github.tomakehurst" %  "wiremock-standalone"  % "3.0.1"    % testScope,
+    "org.scalatestplus"      %% "mockito-3-4"          % "3.2.10.0"  % testScope,
+    "com.vladsch.flexmark"   %  "flexmark-all"         % "0.35.10"   % testScope
   )
 }
