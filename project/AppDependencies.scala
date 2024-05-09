322227eb94547cac7430e41477ee4272ff7bd535
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2024-05-09 10:48:41 +0100

    DCWL-2003 added variable for playVersion

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,14 @@ import sbt.*
 
 object AppDependencies {
   private val bootstrapVersion = "8.5.0"
+  private val playVersion = "play-30"
 
   val compile = Seq(
-    "uk.gov.hmrc"            %% s"bootstrap-backend-play-30" % bootstrapVersion,
-    "org.typelevel"          %% "cats-core"                  % "2.10.0"
+    "uk.gov.hmrc"            %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
+    "org.typelevel"          %% "cats-core"                       % "2.10.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test
+    "uk.gov.hmrc"            %% "bootstrap-test-$playVersion" % bootstrapVersion % Test
   )
 }
