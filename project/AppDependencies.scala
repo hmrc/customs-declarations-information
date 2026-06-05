d7e38359e540074322b1423a9a752bf1880fff3b
Sergi <97970044+sergi-hmrc@users.noreply.github.com>
2026-06-05 12:54:31 +0100

    DCWL-4817: remove vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "10.4.0"
+  private val bootstrapVersion = "10.7.0"
   private val playVersion = "play-30"
 
   val compile = Seq(
